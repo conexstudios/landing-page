c385d454e01a20f63fab15e98a672e5ff6fc51ff
Yotman Reyes <yotman.reyes@gmail.com>
2025-05-28 17:07:03 -0400

    comment stats section

--- app/page.tsx
+++ app/page.tsx
@@ -253,7 +253,7 @@ export default function ConexStudiosLanding() {
       <WaveSection />
 
       {/* Stats Section */}
-      <AnimatedSection>
+      {/* <AnimatedSection>
         <section className="py-16 bg-white">
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="grid grid-cols-2 md:grid-cols-4 gap-8 text-center">
@@ -284,7 +284,7 @@ export default function ConexStudiosLanding() {
             </div>
           </div>
         </section>
-      </AnimatedSection>
+      </AnimatedSection> */}
 
       <WaveSection variant="reverse" />
 
